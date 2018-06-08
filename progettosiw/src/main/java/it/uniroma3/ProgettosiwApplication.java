0c554a6d66f0e3c5aa91053169803c05e5a08161
Gabriele D'Angelo <gabrysaur@gmail.com>
2018-06-08 11:16:42 +0200

    primmo commit

---
+++ progettosiw/src/main/java/it/uniroma3/ProgettosiwApplication.java
@@ -0,0 +1,14 @@
+package it.uniroma3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//per commit
+
+@SpringBootApplication
+public class ProgettosiwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettosiwApplication.class, args);
+	}
+}
